e5d2aa32b13e51017ac968f09875ccced96d8c12
Anton Debner <anton.debner@aalto.fi>
2019-02-02 12:36:56 +0200

    Add painting inspection proto

---
+++ turno-notturno/Assets/Scripts/Clue.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject icon;
+    public float lineWidth;
+
+    private LineRenderer lineRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetDiscovered(false);
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.SetWidth(lineWidth, lineWidth);
+        Vector3 start = icon.transform.position;
+        Vector3 end = text.transform.position;
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(0, end);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimateDiscovery()
+    {
+        // Reveal the clue slowly
+        StartCoroutine("AnimateLine", 0.5f);
+        EnableColliders(false);
+    }
+
+    public void SetDiscovered(bool value)
+    {
+        // Instantly shows or hides the clue
+        
+        if (icon) icon.SetActive(value);
+        if (text) text.SetActive(value);
+        if (lineRenderer) lineRenderer.enabled = value;
+        EnableColliders(!value);  // Colliders should be enabled if the clue has not yet been discovered
+    }
+
+    public void EnableColliders(bool value)
+    {
+        foreach (var collider in GetComponents<Collider>())
+        {
+            collider.enabled = value;
+        }
+    }
+
+
+
+    IEnumerator AnimateLine(float duration)
+    {
+        // Show icon
+        if (icon) icon.SetActive(true);
+        
+        // Animate line
+        Vector3 start = icon.transform.position;
+        Vector3 end = text.transform.position;
+        Vector3 direction = end - start;
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, start);
+        for (float f = 1f; f >= 0; f -= 1/duration * Time.deltaTime) 
+        {
+            lineRenderer.SetPosition(1, start + direction * (1-f));
+            yield return null;
+        }
+        
+        // Show text
+        if (text) text.SetActive(true);
+    }
+}
---
+++ turno-notturno/Assets/Scripts/Painting.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Painting : MonoBehaviour
+{
+
+    public GameObject camera;
+    public GameObject hintIcon;
+    public GameObject cursorIcon;
+    public Text cluesRemaining;
+    public float inspectDistance;
+    public float inspectSpeed;
+    public float maxX;
+    public float maxY;
+    public float maxZoom;
+
+    private bool inspecting = false;
+    private float x;
+    private float y;
+
+    private float zoom = 1;
+
+    private List<Clue> clues;
+    private List<Clue> foundClues;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        clues = new List<Clue>(GetComponentsInChildren<Clue>());
+        foundClues = new List<Clue>();
+        StartInspect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (inspecting)
+        {
+            HandleInspectMovement();
+            
+            // Set zoom
+            if (Input.GetKey(KeyCode.Mouse0)) zoom = maxZoom;
+            else zoom = 1;
+            
+            // Set UI elements
+            if (cluesRemaining)
+            {
+                cluesRemaining.text = (clues.Count - foundClues.Count).ToString();    
+            }
+            
+            hintIcon.SetActive(false);
+
+            // Check for clues with raycast
+            RaycastHit hit;
+            Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+            if (Physics.Raycast(ray, out hit)) 
+            {
+                GameObject objectHit = hit.transform.gameObject;
+                Clue clue = objectHit.GetComponent<Clue>();
+                if(clue != null)
+                {
+                    if (clues.Contains(clue))
+                    {
+                        hintIcon.SetActive(true);
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            foundClues.Add(clue);
+                            clue.AnimateDiscovery();
+                            Debug.Log("Clue found!");
+                        }
+                        else
+                        {
+                            //Debug.Log("Yes clue!");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Clicked on clue that does not belong to this painting!");
+                    }
+                }
+                else
+                {
+                    //Debug.Log("No clue");
+                }
+            }
+            else
+            {
+                //Debug.Log("No hit");
+            }
+            cursorIcon.SetActive(!hintIcon.active);
+        
+            
+        }
+    }
+
+    void StartInspect()
+    {
+        inspecting = true;
+        camera.transform.position = transform.position - transform.forward * inspectDistance;
+        x = 0;
+        y = 0;
+    }
+
+    void StopInspect()
+    {
+        inspecting = false;
+    }
+
+    void HandleInspectMovement()
+    {
+        float vSpeed = 1;
+        float hSpeed = 1;
+
+        float mouseX = -Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+            
+        if (mouseX > 0)
+        {
+            hSpeed = Mathf.Clamp(maxX-x, 0, 1);
+        }
+        if (mouseX < 0)
+        {
+            hSpeed = Mathf.Clamp(maxX+x, 0, 1);
+        }   
+        if (mouseY > 0)
+        {
+            vSpeed = Mathf.Clamp(maxY-y, 0, 1);
+        }
+        if (mouseY < 0)
+        {
+            vSpeed = Mathf.Clamp(maxY+y, 0, 1);
+        }
+            
+        float h = inspectSpeed * hSpeed * mouseX * zoom;
+        float v = inspectSpeed * vSpeed * mouseY * zoom;
+            
+        x = Mathf.Clamp(x+h, -maxX, maxX);
+        y = Mathf.Clamp(y+v, -maxY, maxY);
+
+        camera.transform.position = transform.position - transform.forward * inspectDistance * zoom;
+        camera.transform.position += transform.right * x;
+        camera.transform.position += transform.up * y;
+    }
+    
+    
+}
