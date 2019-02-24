871fc2035dc43e91bdf8bea84bbf96f2a4db24bb
Skylerzhang023 <skyler2zhang@gmail.com>
2019-02-24 02:07:21 +0200

    add first artwork

---
+++ turno-notturno/Assets/Woodcity_rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Woodcity_rotate : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            // Rotate the object around its local X axis at 1 degree per second
+            transform.Rotate(Vector3.up * speed* Time.deltaTime);
+
+            // ...also rotate around the World's Y axis
+           // transform.Rotate(Vector3.up * speed* Time.deltaTime, Space.World);
+    }
+}
