ee5625368f649e78cb22186a324e3c38004fcc7b
tohtoriteppo <tohtoriteppo@hotmail.com>
2019-03-01 10:32:18 +0200

    fixing the bridge minigame

--- turno-notturno/Assets/ArtPieceDragging.cs
+++ turno-notturno/Assets/ArtPieceDragging.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class ArtPieceDragging : MonoBehaviour
 {
+    private void OnMouseDown()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
 
+    private void OnMouseDrag()
+    {
+        //Debug.Log(Input.mousePosition);
+        Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        transform.position = new Vector3(pos.x, pos.y, 0);
+        Debug.Log(transform.position);
+    }
 
+    private void OnMouseUp()
+    {
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
 }
