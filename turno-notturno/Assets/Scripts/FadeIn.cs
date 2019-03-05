0e2bd843e983cf9b926aedd66cca7a5d3756ff24
tohtoriteppo <tohtoriteppo@hotmail.com>
2019-03-05 16:27:04 +0200

    Made 1st minigame smoother with transitions, added act1 functionality to Görkem's scene

--- turno-notturno/Assets/Scripts/FadeIn.cs
+++ turno-notturno/Assets/Scripts/FadeIn.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class FadeIn : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [SerializeField] float fadeTime;
     [SerializeField] bool fadeIn;
+    [SerializeField] bool changeSceneAfterDone;
 
-    private int counter;
+    private float timer;
 
     private void Start()
     {
@@ -21,9 +22,16 @@ public class FadeIn : MonoBehaviour
     void Update()
     {
         Color color = GetComponent<Image>().color;
-        float alpha = fadeIn == true ? 1 - counter * speed : counter * speed;
+        float fade = timer / fadeTime;
+        //float slow = fade < 0.5f ? 0.5f : 1;
+        float alpha = fadeIn == true ? 1 -  fade: fade;
         GetComponent<Image>().color = new Color(color.r, color.g, color.b, alpha);
-        counter++;
+        timer += Time.deltaTime;
+        if(timer > fadeTime && changeSceneAfterDone)
+        {
+            GameManager gameManager = GameManager.GetInstance();
+            gameManager.LoadNextAct();
+        }
 
     }
 }
