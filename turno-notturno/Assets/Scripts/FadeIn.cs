d8583b29a032bfdb238ab640c72548193e610f68
Anton Debner <anton.debner@aalto.fi>
2019-03-05 22:19:24 +0200

    Revert "merge"
    This reverts commit 2e770897cd13eed86b1fcb27cb27fb4ec57a2171, reversing
    changes made to ae715b0b61957385c315151777dc97a6b23c3a47.

---
+++ turno-notturno/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField] float fadeTime;
+    [SerializeField] bool fadeIn;
+    [SerializeField] bool changeSceneAfterDone;
+
+    private float timer;
+
+    private void Start()
+    {
+        Color color = GetComponent<Image>().color;
+        int var = fadeIn == true ? 1 : 0;
+        GetComponent<Image>().color = new Color(color.r, color.g, color.b, var);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color color = GetComponent<Image>().color;
+        float fade = timer / fadeTime;
+        //float slow = fade < 0.5f ? 0.5f : 1;
+        float alpha = fadeIn == true ? 1 -  fade: fade;
+        GetComponent<Image>().color = new Color(color.r, color.g, color.b, alpha);
+        timer += Time.deltaTime;
+        if(timer > fadeTime && changeSceneAfterDone)
+        {
+            GameManager gameManager = GameManager.GetInstance();
+            gameManager.LoadNextAct();
+        }
+
+    }
+}
