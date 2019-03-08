8f7d0601d11dc65b9cad163c30f1d62d1ec60cc7
Skylerzhang023 <skyler2zhang@gmail.com>
2019-03-08 13:05:33 +0200

    add migrain

---
+++ turno-notturno/Assets/MigrainEffect.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class MigrainEffect : MonoBehaviour
+{
+    public PostProcessProfile Migraint;
+    public float speed;
+    public bool ismigrain;
+    private bool isincreasing;
+    public float EffectStrength;
+    ChromaticAberration chromeaticthing;
+    AmbientOcclusion AmbientOcclusionthing;
+    Grain grainthing;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        ismigrain = false;
+        isincreasing = false;
+        Migraint.TryGetSettings(out chromeaticthing);
+        Migraint.TryGetSettings(out grainthing);
+        Migraint.TryGetSettings(out AmbientOcclusionthing);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        // judge the state
+        if (ismigrain)
+        {
+            if (chromeaticthing.intensity.value >= EffectStrength)
+                isincreasing = false;
+            if (chromeaticthing.intensity.value <= 0)
+                isincreasing = true;
+
+            if (isincreasing)
+            {
+                chromeaticthing.intensity.value += speed;
+                grainthing.intensity.value += speed;
+                AmbientOcclusionthing.intensity.value += speed*4;
+            }
+            else
+            {
+                chromeaticthing.intensity.value -= speed;
+                grainthing.intensity.value -= speed;
+                AmbientOcclusionthing.intensity.value += speed * 4;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartMigrain();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            EndMigrain();
+        }
+    }
+    public void StartMigrain() {
+        ismigrain = true;
+    }
+    public void EndMigrain()
+    {
+        ismigrain = false;
+        ResetValue();
+    }
+    public void ResetValue()
+    { 
+        isincreasing = false;
+        chromeaticthing.intensity.value = 0;
+    }
+}
