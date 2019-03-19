7a52cb5770bc7ba1c5d60b27ac4c759b506406d7
tohtoriteppo <tohtoriteppo@hotmail.com>
2019-03-19 16:30:02 +0200

    added fading to migraine

--- turno-notturno/Assets/MigrainEffect.cs
+++ turno-notturno/Assets/MigrainEffect.cs
@@ -10,33 +10,41 @@ public class MigrainEffect : MonoBehaviour
     public float speed;
     public bool ismigrain;
     private bool isincreasing;
+    public float fadeSpeed;
+    private float maximumSound = 0.55f;
     public float EffectStrength;
+    private float effectLimit = 0;
+    private float soundVolume = 0;
+    private PostProcessVolume ppVolume;
     ChromaticAberration chromeaticthing;
     AmbientOcclusion AmbientOcclusionthing;
     Grain grainthing;
     // Start is called before the first frame update
     void Start()
     {
+        ppVolume = GetComponent<PostProcessVolume>();
         ismigrain = false;
         isincreasing = false;
         Migraint.TryGetSettings(out chromeaticthing);
         Migraint.TryGetSettings(out grainthing);
         Migraint.TryGetSettings(out AmbientOcclusionthing);
         EndMigrain();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        // judge the state
+
+        
+
         if (ismigrain)
         {
-            if (chromeaticthing.intensity.value >= EffectStrength)
+            // judge the state
+            if (chromeaticthing.intensity.value >= effectLimit)
                 isincreasing = false;
             if (chromeaticthing.intensity.value <= 0)
                 isincreasing = true;
-
             if (isincreasing)
             {
                 chromeaticthing.intensity.value += speed;
@@ -47,36 +55,52 @@ public class MigrainEffect : MonoBehaviour
             {
                 chromeaticthing.intensity.value -= speed;
                 grainthing.intensity.value -= speed;
-                //AmbientOcclusionthing.intensity.value += speed;
+                //AmbientOcclusionthing.intensity.value -= speed*4;
             }
+
+
+
+            ppVolume.weight = Mathf.Min(ppVolume.weight + fadeSpeed, 1);
+            effectLimit = Mathf.Min(effectLimit + fadeSpeed * EffectStrength, EffectStrength);
+            soundVolume = Mathf.Min(soundVolume + fadeSpeed * maximumSound, maximumSound);
+            Camera.main.GetComponent<StudioEventEmitter>().EventInstance.setParameterValue("migraineVolume", soundVolume);
+        } 
+        else
+        {
+            ppVolume.weight = Mathf.Max(ppVolume.weight - fadeSpeed, 0);
+            effectLimit = Mathf.Max(effectLimit - fadeSpeed * EffectStrength, 0);
+            soundVolume = Mathf.Max(soundVolume - fadeSpeed * maximumSound, 0);
+            Camera.main.GetComponent<StudioEventEmitter>().EventInstance.setParameterValue("migraineVolume", soundVolume);
+
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            //StartMigrain();
+           // StartMigrain();
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
-            //EndMigrain();
+           // EndMigrain();
         }
     }
     public void StartMigrain() {
-        Camera.main.GetComponent<StudioEventEmitter>().enabled = true;
+       // Camera.main.GetComponent<StudioEventEmitter>().enabled = true;
         ismigrain = true;
         PostProcessVolume ppVolume = GetComponent<PostProcessVolume>();
-        if (ppVolume) ppVolume.enabled = true;
     }
     public void EndMigrain()
     {
-        Camera.main.GetComponent<StudioEventEmitter>().enabled = false;
+        //Camera.main.GetComponent<StudioEventEmitter>().enabled = false;
         ismigrain = false;
         PostProcessVolume ppVolume = GetComponent<PostProcessVolume>();
-        if (ppVolume) ppVolume.enabled = false;
-        ResetValue();
+        ResetValues();
     }
-    public void ResetValue()
-    { 
-        isincreasing = false;
+
+    public void ResetValues()
+    {
         chromeaticthing.intensity.value = 0;
+        grainthing.intensity.value = 0;
+        AmbientOcclusionthing.intensity.value = 4;
+        isincreasing = true;
     }
 
     public void StartMigrainDelayed(float delay)
