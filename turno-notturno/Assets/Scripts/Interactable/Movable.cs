2db021a255555986987b3221dd235233bb930054
Anton Debner <anton.debner@aalto.fi>
2019-04-04 18:51:10 +0300

    Clean up script folder

---
+++ turno-notturno/Assets/Scripts/Interactable/Movable.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movable : Interactable
+{
+    [SerializeField] float throwVelocity = 20f;
+    public string name;
+    private bool playerIsHolding = false;
+    
+    private Transform target;
+    private float targetDistance;
+    private Rigidbody rbody;
+    private bool gravityWasEnabled;
+    private int originalLayer;
+    private bool wasHoldingThisFrame;
+
+    void Awake()
+    {
+        originalLayer = gameObject.layer;
+        rbody = GetComponent<Rigidbody>();
+        rbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wasHoldingThisFrame = playerIsHolding;
+        if (playerIsHolding)
+        {
+            Vector3 targetPos = target.position + target.forward * targetDistance;
+            Vector3 offset = targetPos - GetCenterOfMass();
+            //rbody.AddForce(offset*10, ForceMode.Acceleration);
+            rbody.velocity = offset * 10;
+            rbody.angularVelocity = Vector3.zero;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Throw();
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Drop();
+            }
+        }
+        renderer.material.shader = originalShader;
+    }
+
+    Vector3 GetCenterOfMass()
+    {
+        return transform.TransformPoint(rbody.centerOfMass);
+    }
+
+    public void Grab()
+    {
+        target = Camera.main.transform;
+        targetDistance = (target.position - GetCenterOfMass()).magnitude;
+        playerIsHolding = true;
+        gravityWasEnabled = rbody.useGravity;
+        rbody.useGravity = false;
+        IgnorePlayerCollision(true);
+    }
+
+    public void Drop()
+    {
+        playerIsHolding = false;
+        rbody.useGravity = gravityWasEnabled;
+        IgnorePlayerCollision(false);
+        wasHoldingThisFrame = true;
+    }
+
+    void IgnorePlayerCollision(bool value)
+    {
+        // Ignore collision between movable object and player to avoid flying while holding the object
+        //if (value) gameObject.layer = LayerMask.NameToLayer("Player");
+        //else gameObject.layer = originalLayer;
+    }
+
+    public void Throw()
+    {
+        Drop();
+        rbody.velocity = target.forward * throwVelocity;
+    }
+
+    public override void OnInteract()
+    {
+        if (wasHoldingThisFrame)
+        {
+            // Avoid picking the object up immediately after dropping it
+            return;
+        }
+        base.OnInteract();
+        if (!playerIsHolding) Grab();
+        else Drop();
+    }
+
+    public override string GetTooltip()
+    {
+        if (!playerIsHolding) return "pick up " + name;
+        else return "Left click to Drop \nE to throw";
+    }
+}
---
+++ turno-notturno/Assets/Scripts/headbobber.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headbobber : MonoBehaviour
+{
+    public AudioClip[] WalkNoises;
+    public float bobbingSpeed = 0.18f;
+    public float bobbingAmount = 0.2f;
+    public float midpoint = 2.0f;
+    private float waveslice,horizontal, vertical, translateChange, totalAxes;
+    
+    private float timer = 0.0f;
+    private Player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        InvokeRepeating("PlaySound", 0.0f, 0.5f);
+    }
+    void Update()
+    {
+        if (!player || !player.enabled) return; // Disable bobbing on pause menu and while inspecting paintings
+        waveslice = 0f;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
+        {
+            timer = 0.0f;
+        }
+        else
+        {
+            waveslice = Mathf.Sin(timer);
+            timer = timer + bobbingSpeed;
+            if (timer > Mathf.PI * 2)
+            {
+                timer = timer - (Mathf.PI * 2);
+            }
+        }
+        if (waveslice != 0)
+        {
+            translateChange = waveslice * bobbingAmount;
+            totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+            totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
+            translateChange = totalAxes * translateChange;
+            transform.localPosition =  new Vector3(0, midpoint+translateChange, 0);
+
+
+           
+        }
+        else
+        {
+            transform.localPosition =new Vector3(0, midpoint, 0);
+
+        }
+    }
+    void PlaySound()
+    {
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            SoundManager.Instance.RandomSoundEffect(WalkNoises);
+        }
+    }
+}
