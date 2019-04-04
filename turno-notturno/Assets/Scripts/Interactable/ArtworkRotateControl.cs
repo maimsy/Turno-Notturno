1454f5f3b8f808cd7b6def310c7995ec579f3fe2
Anton Debner <anton.debner@aalto.fi>
2019-04-05 00:34:41 +0300

    Fix first artwork rotation button

---
+++ turno-notturno/Assets/Scripts/Interactable/ArtworkRotateControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtworkRotateControl : BaseInteractable
+{
+    [SerializeField] Rotate artworkRotate;
+    [SerializeField] string stopTooltip = "Stop rotation";
+    [SerializeField] string startTooltip = "Start rotation";
+
+    public override void Interact()
+    {
+        if (artworkRotate)
+        {
+            artworkRotate.rotationEnabled = !artworkRotate.rotationEnabled;
+        }
+    }
+
+    public override string GetTooltip()
+    {
+        if (artworkRotate && artworkRotate.rotationEnabled)
+        {
+            return stopTooltip;
+        }
+        else if (artworkRotate && !artworkRotate.rotationEnabled)
+        {
+            return startTooltip;
+        }
+        else
+        {
+            return "Broken plz fix";
+        }
+    }
+}
