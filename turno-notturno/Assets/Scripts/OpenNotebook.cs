bf78e3c0227008a796f991e479525613510defbe
maimsy <jhilum@hotmail.com>
2019-04-18 15:04:03 +0300

    Note stuff

---
+++ turno-notturno/Assets/Scripts/OpenNotebook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenNotebook : MonoBehaviour
+{  
+    public GameObject Canvas;
+
+    private void OnEnable()
+    {
+        Debug.Log("Hellloo");
+        //StartCoroutine("ShowText", 4);
+    }
+
+    private IEnumerator ShowNoteBookText(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Canvas.SetActive(true); 
+    }
+
+    private void OnDisable()
+    {
+        Canvas.SetActive(false);
+    }
+
+}
